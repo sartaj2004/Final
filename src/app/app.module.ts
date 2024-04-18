8685308123f8adf70781f134ed1c5b8c6a410188
Sartaj Singh <sin14136@sheridancollege.ca>
2024-04-18 08:24:45 -0400

    Components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { CollegeComponent } from './college/college.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    CollegeComponent
   ],
   imports: [
     BrowserModule,
